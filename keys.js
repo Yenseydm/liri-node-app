abecc2ba77062d309138bd12b8b642ca9e14008b
Yensey Murillo <yenseymurillo@gmail.com>
2020-02-15 09:57:20 -0600

    spotify progess

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-console.log("this is loaded");
+console.log('this is loaded');
 
 exports.spotify = {
-    id: 78a3f097b0c74020aac8cf62eec66d0c,
-    secret: ed78efe2db294140bc20dc3b9242ab3e
-  };
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,12 +1,6 @@
 require("dotenv").config();
 
-// var keys = require("./keys.js");
-
-// var Spotify = require("node-spotify-api");
-
-// var spotify = new Spotify(keys.spotify);
-
-
+var keys = require("./keys.js");
 
 var command = process.argv[2];
 
@@ -29,34 +23,60 @@ switch (command) {
                 break;
 }
 
+// Bands in town axios
 function concerts() {
         var axios = require("axios");
         var artist = process.argv[3];
 
-        var queryUrl = "https://rest.bandsintown.com/artists/" + artist+ "/events?app_id=codingbootcamp";
+        var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
         if (command === "concert-this") {
 
                 axios.get(queryUrl).then(
-                        function(response) {
-                                for ( i = 0; i < response.data.length; i++){
-                          console.log("------------------------------");
-                          console.log("Artist: " + response.data[1].lineup);
-                        console.log("Venue: " + response.data[1].venue.name)
-                          console.log("Event Date: " + response.data[1].datetime);
-                          console.log("------------------------------")
+                        function (response) {
+                                for (i = 0; i < response.data.length; i++) {
+                                        console.log("------------------------------");
+                                        console.log("Artist: " + response.data[i].lineup);
+                                        console.log("Venue: " + response.data[i].venue.name)
+                                        console.log("Event Date: " + response.data[i].datetime);
+                                        console.log("------------------------------")
                                 }
-                        })
-        } 
-};
-
-function music() {
-
-        if (command === "spotify-this-song") {
-                console.log("rock on")
+                        }).catch(function (error) {
+                                console.error('Error occurred: ' + error);
+                        });
         }
 };
 
+//Spotify API
+function music() {
+        var Spotify = require("node-spotify-api");
+
+        var spotify = new Spotify(keys.spotify);
+
+        var songs = process.argv[3];
+
+        if (command === "spotify-this-song") {
+        
+        spotify
+        .search({ type: 'track', query: songs })
+        .then(function(response) {
+        //   console.log("Artist: ");
+        //   console.log("Song: ");
+        //   console.log("Album: ")
+        //   console.log("Preview: ");
+
+        console.log(JSON.stringify(response.tracks))
+          
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+
+        }
+};
+
+
+// OMDB Axios
 function movies() {
         var axios = require("axios");
         var film = process.argv[3];
@@ -66,35 +86,37 @@ function movies() {
         if (command === "movie-this") {
 
                 axios.get(queryUrl).then(
-                        function(response) {
-                          console.log("------------------------------");
-                          console.log("Title: " + response.data.Title);
-                          console.log("Year: " + response.data.Year);
-                          console.log("The movie's rating is: " + response.data.imdbRating);
-                          console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-                          console.log("Country produced: " + response.data.Country);
-                          console.log("Language: " + response.data.Language);
-                          console.log("Plot: " + response.data.Plot);
-                          console.log("Actors: " + response.data.Actors);
-                          console.log("------------------------------")
+                        function (response) {
+                                console.log("------------------------------");
+                                console.log("Title: " + response.data.Title);
+                                console.log("Year: " + response.data.Year);
+                                console.log("The movie's rating is: " + response.data.imdbRating);
+                                console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+                                console.log("Country produced: " + response.data.Country);
+                                console.log("Language: " + response.data.Language);
+                                console.log("Plot: " + response.data.Plot);
+                                console.log("Actors: " + response.data.Actors);
+                                console.log("------------------------------")
                         })
-                        .catch(function(error) {
-                          if (error.response) {
-                            console.log("---------------Data---------------");
-                            console.log(error.response.data);
-                            console.log("---------------Status---------------");
-                            console.log(error.response.status);
-                            console.log("---------------Status---------------");
-                            console.log(error.response.headers);
-                          } else if (error.request) {
-                           console.log(error.request);
-                          } else {
-                            console.log("Error", error.message);
-                          }
-                          console.log(error.config);
+                        .catch(function (error) {
+                                if (error.response) {
+                                        console.log("---------------Data---------------");
+                                        console.log(error.response.data);
+                                        console.log("---------------Status---------------");
+                                        console.log(error.response.status);
+                                        console.log("---------------Status---------------");
+                                        console.log(error.response.headers);
+                                } else if (error.request) {
+                                        console.log(error.request);
+                                } else {
+                                        console.log("Error", error.message);
+                                }
+                                console.log(error.config);
                         });
         }
 };
+
+
 function says() {
         if (command === "do-what-it-says") {
                 console.log("do something please")
